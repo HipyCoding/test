4fc2ea6f1986fa0c929f26a0bd2c3681af1dd747
Candrese <candrese@student.42Heilbronn.de>
2024-06-16 09:35:35 +0200

    function to fill stack b with values not needed anymore. was to test operators

--- handle_args.c
+++ handle_args.c
@@ -6,7 +6,7 @@
 /*   By: candrese <candrese@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/12 16:20:03 by candrese          #+#    #+#             */
-/*   Updated: 2024/06/10 18:54:16 by candrese         ###   ########.fr       */
+/*   Updated: 2024/06/16 03:59:35 by candrese         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,17 +65,3 @@ t_stack_node	*read_args(int argc, char **argv)
 		free_and_exit(NULL, a, 1);
 	return (a);
 }
-
-// initializes b stack for testing
-// t_stack_node *argsb ()
-// {
-// 	t_stack_node 	*b;
-// 	int	i = 10;
-
-// 	b = NULL;
-// 	while(i<15)
-// 	{
-// 		add_node(&b, i++);
-// 	}
-// 	return (b);
-// }
