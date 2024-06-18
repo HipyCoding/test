0c1d582b31fcfa55cdc86b18282d0066d910e8be
Candrese <candrese@student.42Heilbronn.de>
2024-06-18 06:18:32 +0200

    file was renamed from handle_args.c || moved add_node function to the functions, which were in previous 'handle_args' now 'parse_args', since it was the only function in its previous file

--- parse_args.c
+++ parse_args.c
@@ -1,16 +1,44 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   init_to_winit.c                                    :+:      :+:    :+:   */
+/*   handle_args.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: candrese <candrese@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/12 16:55:02 by candrese          #+#    #+#             */
-/*   Updated: 2024/06/14 20:27:16 by candrese         ###   ########.fr       */
+/*   Created: 2024/05/12 16:20:03 by candrese          #+#    #+#             */
+/*   Updated: 2024/06/18 06:04:45 by candrese         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+void	dup_check(t_stack_node *stack, int *error)
+{
+	t_stack_node	*check;
+	t_stack_node	*node;
+
+	if (!stack)
+	{
+		*error = 1;
+		return;
+	}
+	check = NULL;
+	node = stack;
+	while (node != NULL)
+	{
+		check = node->next;
+		while (check != NULL)
+		{
+			if (node->value == check->value)
+			{
+				*error = 1;
+				return;
+			}
+			check = check->next;
+		}
+		node = node->next;
+	}
+}
 
 void	add_node (t_stack_node **stack, int value)
 {
@@ -37,3 +65,28 @@ void	add_node (t_stack_node **stack, int value)
 	}
 }
 
+t_stack_node	*read_args(int argc, char **argv)
+{
+	t_stack_node	*a;
+	int				i;
+	int				error;
+
+	error = 0;
+	i = 1;
+	a = NULL;
+	if (argc == 2)
+	{
+		i = 0;
+		argv = ft_split(argv[1], ' ');
+		if (!argv)
+			free_and_exit(NULL, &a, 1);
+	}
+	while (argv[i])
+		add_node(&a, my_atolong(argv[i++], &error));
+	if (argc == 2)
+		free_2d_string(argv);
+	dup_check(a, &error);
+	if (error == 1)
+		free_and_exit(NULL, &a, 11);
+	return (a);
+}
