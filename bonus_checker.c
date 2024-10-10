5b6be1ef8f84182c586046a3cc483e9e5983b9b7
Candrese <candrese@student.42Heilbronn.de>
2024-10-10 02:04:23 +0200

    we need a new main function, first we start by parsing the arguments just the same way as we do in our main function from the mandatory part

---
+++ bonus_checker.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   bonus_checker.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: candrese <candrese@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/10 01:58:29 by candrese          #+#    #+#             */
+/*   Updated: 2024/10/10 02:01:54 by candrese         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int		main(int argc, char **argv)
+{
+	t_stack_node	*a;
+	t_stack_node	*b;
+
+	a = NULL;
+	b = NULL;
+	if (argc == 1 || (argc == 2 && !argv[1][0]))
+		return (1);
+	a = read_args(argc, argv);
+	return (0);
+}
