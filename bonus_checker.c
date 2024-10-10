901db4904b5d8f50f99f44d7b6206b86ea1c87f3
Candrese <candrese@student.42Heilbronn.de>
2024-10-10 02:10:53 +0200

    put a pointer to the input string until a \n, using get_next_line

--- bonus_checker.c
+++ bonus_checker.c
@@ -6,21 +6,25 @@
 /*   By: candrese <candrese@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/10 01:58:29 by candrese          #+#    #+#             */
-/*   Updated: 2024/10/10 02:01:54 by candrese         ###   ########.fr       */
+/*   Updated: 2024/10/10 02:07:19 by candrese         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-
+#include <stdio.h>
 int		main(int argc, char **argv)
 {
 	t_stack_node	*a;
 	t_stack_node	*b;
+	char			*input;
 
 	a = NULL;
 	b = NULL;
 	if (argc == 1 || (argc == 2 && !argv[1][0]))
 		return (1);
 	a = read_args(argc, argv);
+	input = get_next_line(0);
+	printf("%s",input);
+	
 	return (0);
 }
